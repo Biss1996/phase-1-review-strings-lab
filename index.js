3124a6d7213f0c9a5c775615b95f1bdfc0013c16
Bismark <bettkipkoech45@gmail.com>
2025-03-11 14:58:25 +0300

    short greeting

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+
+const currentUser = "Grace Hopper";
+
+// Fixing welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Fixing excitedWelcomeMessage
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Fixing shortGreeting
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+// Exporting variables for testing
+module.exports = {
+  currentUser,
+  welcomeMessage,
+  excitedWelcomeMessage,
+  shortGreeting
+};
